85d75096c220c8569b60f6ec03df28e21d5740be
kenny <879464294@qq.com>
2021-12-23 17:39:36 +0800

    修复一下

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
+// eslint-disable-next-line no-unused-vars
 import Vue from 'vue'
 import './app.less'
 
 const App = {
   onShow (options) {
+    console.log(options)
   },
   render(h) {
     // this.$slots.default 是将要会渲染的页面
