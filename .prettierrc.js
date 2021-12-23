399e253022b694065ffb441e487925e2aedd57c0
kenny <879464294@qq.com>
2021-12-23 17:35:52 +0800

    feat: initial project

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  // 代码结尾禁用分号
+  semi: false,
+
+  // 每行代码最大长度
+  printWidth: 100,
+}
---
+++ src/pages/index/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import './index.less'
+
+export default {
+  data () {
+    return {
+      msg:  'Hello world!'
+    }
+  },
+
+
+  created() {
+    const a = () => {
+      return 3
+    }
+    console.log(999, a)
+  }
+
+
+}
+</script>
