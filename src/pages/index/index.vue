decfac93a52042996522c15d5e433c6dfde83b48
kenny <879464294@qq.com>
2021-12-24 17:56:43 +0800

    增加login页面

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -11,6 +11,8 @@ export default {
   data () {
     return {
       msg:  'Hello world!'
+
+      
     }
   },
 
---
+++ src/pages/login/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <view class="login">
+    <view class="footer">
+      <view class="treaty">
+        登录即表明您已同意<text>《U社用户服务协议》</text>
+      </view>
+
+      <view class="btn">
+        微信登录
+      </view>
+
+      <view class="phone_login">
+        手机号登录
+      </view>
+    </view>
+  </view>
+</template>
+
+
+<script>
+import './index.less'
+
+export default {
+    
+}
+</script>
